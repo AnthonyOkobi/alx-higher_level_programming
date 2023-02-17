49412c36e71322d5218609d39ad1cf5f08e569f1
AnthonyOkobi <okobitony@gmail.com>
2023-02-17 09:20:13 +0000

    Correction

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,49 +1,95 @@
 #!/usr/bin/python3
-
-"""This module defines a square
-It inherits from the ``Rectangle`` class in rectangle.py
 """
+Inherits from Rectangle;
+Inits superclass' id, width (as size), height (as size), x, y
+Contains public attribute size
+Prints [Square] (<id>) <x>/<y> - <size>
+Updates attributes: arg1=id, arg2=size, arg3=x, arg4=y
+Returns dictionary representation of attributes
+"""
+
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """defines a square"""
-
+    """
+    defines class Square; inherits from class Rectangle
+    Inherited Attributes:
+        id
+        __weight        __height
+        __x             __y
+    Class Attributes:
+        size
+    Inherted Methods:
+        Base.init(self, id=None)
+        Rectangle.init(self, width, height, x=0, y=0, id=None)
+        update(self, *args, **kwargs)
+        width(self)      width(self, value)
+        height(self)     height(self, value)
+        x(self)          x(self, value)
+        y(self)          y(self, value)
+        area(self)       display(self)
+    Methods:
+        __str__
+        __init__(self, size, x=0, y=0, id=None)
+        update(self, *args, **kwargs)
+        size(self)       size(self, value)
+        to_dictionary(self)
+    """
     def __init__(self, size, x=0, y=0, id=None):
+        """Initialize square"""
         super().__init__(size, size, x, y, id)
-
-    def __str__(self):
-        """updates the string representation of a square
-        Returns:
-            the string representation of a square
-        """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-
-    def to_dictionary(self):
-        """returns a dictionary representation of a square"""
-        return {"id": self.id, "size": self.width, "x": self.x, "y": self.y}
+        self.size = size
 
     @property
     def size(self):
-        """gets the size attribute of a sqquare"""
+        """Getter size"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """sets the size attribute of a sqquare"""
-        self.validate_attr(value, "width", 1)
+        """Setter size - sets width and height as size"""
         self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Prints [Square] (<id>) <x>/<y> - <size>"""
+        return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
+            self.__class__.__name__, self.id, self.x, self.y,
+            self.size)
 
     def update(self, *args, **kwargs):
-        """updates the attributes of a rectangle instance"""
+        """
+        If args: set attributes in this order: id, width, height, x, y
+        If no args given: set attributes according to kwargs
+        """
+        if args:
+            for key, value in enumerate(args):
+                if key == 0:
+                    self.id = value
+                elif key == 1:
+                    self.size = value
+                elif key == 2:
+                    self.x = value
+                else:
+                    self.y = value
+        else:
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.id = v
+                if k == "size":
+                    self.size = v
+                if k == "x":
+                    self.x = v
+                if k == "y":
+                    self.y = v
 
-        i = 0
-        if kwargs and len(kwargs) > 0:
-            for arg_name, arg in kwargs.items():
-                setattr(self, arg_name, arg)
-        attr = ("id", "size", "x", "y")
-        for arg in args:
-            setattr(self, attr[i], arg)
-            i += 1
-
+    def to_dictionary(self):
+        """Return dictionary representation"""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
